c7456562ee1c622a5f6e49b45faa5baf6a0441fe
chenshuanlin <cslingrace@gmail.com>
2023-12-17 15:10:50 +0800

    整理資料夾

--- Good/thumbnail.js
+++ Good/thumbnail.js
@@ -1,4 +1,4 @@
-const images = ["greentea.jpg", "MoriMaTea.jpg", "teatop.jpg"]; // Add more image URLs as needed
+const images = ["image/greentea.jpg", "image/greentea.jpg", "image/greentea.jpg"]; // Add more image URLs as needed
     let currentImageIndex = 0;
 
     function changeImage(offset) {
