5ae48e80b4bedda2da4da02aa95dc7ea613d59f9
weiting93116 <weiting93116@gmail.com>
2023-12-29 01:56:37 +0800

    botton修改

--- JS/cart.js
+++ JS/cart.js
@@ -60,4 +60,20 @@ const cartItems = [
   }
   
   updateCart();
+
+  //搜尋
+document.addEventListener("DOMContentLoaded", function() {
+  var searchBtn = document.getElementById("searchbtn");
+  var searchBox = document.getElementById("searchBox");
+  var closeSearchBoxBtn = document.getElementById("closeSearchBox");
+
+  searchBtn.addEventListener("click", function() {
+      searchBox.style.display = "block";
+  });
+
+  closeSearchBoxBtn.addEventListener("click", function() {
+      searchBox.style.display = "none";
+  });
+});
+
   
